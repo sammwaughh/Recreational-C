ce14ef4eb63f38b83d36eddca87ebef58d681271
Samuel Waugh <samjmwaugh@gmail.com>
2022-10-25 00:16:02 +0100

    made reverse integer array function

---
+++ reverseArray.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(int nums[], int len);
+
+int main(int argc, char *argv[]) {
+    int nums[9] = {34, 56, 54, 32, 67, 89, 90, 32, 21};
+    printArray(nums, 9);
+    reverse(nums, 9);
+    printArray(nums, 9);
+    return 0;
+}
+
+void printArray(int nums[], int len) {
+    int i;
+    for (i = 0; i < len-1; i++) {
+        printf("%d, ", nums[i]);
+    }
+    printf("%d\n", nums[i]);
+}
+
+void reverse(int nums[], int len) {
+    int i = 0;
+    int j = len;
+    int t;
+    while (i < j) {
+        t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+        i++;
+        j--;
+    }
+}
